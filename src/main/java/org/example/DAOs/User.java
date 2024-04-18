a31611aec5c035177b517a42a41c064ab425606b
ganyuvu <eeiliakas@gmail.com>
2024-04-18 15:15:42 +0100

    Fixed database, added a user table so the user is prompted to log in

---
+++ src/main/java/org/example/DAOs/User.java
@@ -0,0 +1,55 @@
+package org.example.DAOs;
+
+public class User
+{
+    private int id;
+    private String username;
+    private String password;
+    private String displayName;
+    private boolean isAdmin;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+}
--- src/main/java/org/example/MainApp.java
+++ src/main/java/org/example/MainApp.java
@@ -1,20 +1,21 @@
 package org.example;
 
 import org.example.DAOs.DAO;
+import org.example.DAOs.User;
 import org.example.DTOs.Movie;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Scanner;
 
 /**
  * Main author: Joseph Byrne
  * Other contributors: Ema Eiliakas
- *
  */
 
 public class MainApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         Scanner keyboard = new Scanner(System.in);
         DAO dao = DAO.getInstance();
@@ -22,89 +23,106 @@ public class MainApp {
         System.out.println("Hello there and welcome to our movie database");
 
         int choice;
-        do {
-            System.out.println("------------------------");
-            System.out.println("\n1. View all movies.");
-            System.out.println("2. Insert New Movie.");
-            System.out.println("3. Find Movie by ID.");
-            System.out.println("4. Delete Movie by movie ID.");
-            System.out.println("5. Update a movies rating.");
-            System.out.println("6. Filter by rating.");
-            System.out.println("7. Json Convert");
-            System.out.println("8. Exit Code.");
-            System.out.println("------------------------");
 
-            choice = keyboard.nextInt();
-            keyboard.nextLine();
+        System.out.println("Username: ");
+        String userName = keyboard.nextLine();
 
-            switch (choice) {
+        System.out.println("Password: ");
+        String userPassword = keyboard.nextLine();
 
-                case 1:
-                    System.out.println("Showing all movies.");
-                    ShowMovies(dao);
+        User user = dao.logIn(userName,userPassword);
 
-                    break;
+        if(user == null){
+            System.out.println("Login Failed. Invalid User.");
+        }
+        else{
+            do {
+                System.out.println("------------------------");
+                System.out.println("\n1. View all movies.");
+                if(user.isAdmin()){
+                    System.out.println("2. Insert New Movie.");
+                }
+                System.out.println("3. Find Movie by ID.");
+                if(user.isAdmin()){
+                    System.out.println("4. Delete Movie by movie ID.");
+                    System.out.println("5. Update a movies rating.");
+                }
+                System.out.println("6. Filter by rating.");
+                System.out.println("7. Json Convert");
+                System.out.println("8. Exit Code.");
+                System.out.println("------------------------");
 
-                case 2:
-                    System.out.println("You are inserting a new movie.");
-                    Movie newMovie = insertNewMovie(keyboard);
-                    dao.insertMovie(newMovie);
-                    System.out.println("New Movie Inserted");
+                choice = keyboard.nextInt();
+                keyboard.nextLine();
 
-                    break;
+                switch (choice) {
 
-                case 3:
-                    System.out.println("Finding a movie by ID");
-                    System.out.println("Please Enter the Movie ID");
-                    int movieId = keyboard.nextInt();
-                    Movie foundMovie = dao.findMovieById(movieId);
+                    case 1:
+                        System.out.println("Showing all movies.");
+                        ShowMovies(dao);
 
-                    if(foundMovie != null){
+                        break;
 
-                        System.out.println("Movie Found!");
+                    case 2:
+                        System.out.println("You are inserting a new movie.");
+                        Movie newMovie = insertNewMovie(keyboard);
+                        dao.insertMovie(newMovie);
+                        System.out.println("New Movie Inserted");
 
-                        System.out.printf("%-5s %-20s %-12s %-22s %-24s %-12s %-10s\n",
-                                "ID", "Title", "Release", "Genre", "Director", "Runtime", "Rating");
+                        break;
 
-                        System.out.printf("%-5d %-20s %-12d %-22s %-24s %-12d %-10f\n",
-                                foundMovie.getMovie_id(), foundMovie.getTitle(), foundMovie.getRelease_year(),
-                                foundMovie.getGenre(), foundMovie.getDirector(),foundMovie.getRuntime_minutes(),foundMovie.getRating());
-                    }
-                    else
-                    {
-                        System.out.println("No Movie found with this ID!");
-                    }
+                    case 3:
+                        System.out.println("Finding a movie by ID");
+                        System.out.println("Please Enter the Movie ID");
+                        int movieId = keyboard.nextInt();
+                        Movie foundMovie = dao.findMovieById(movieId);
 
-                    break;
+                        if(foundMovie != null){
 
-                case 4:
-                    System.out.println("Enter the movie ID to delete it: ");
-                    int movieIdToDelete = keyboard.nextInt();
-                    keyboard.nextLine();
-                    dao.deleteMovie(movieIdToDelete);
-                    System.out.println("Movie Deleted successfully");
+                            System.out.println("Movie Found!");
 
-                    break;
+                            System.out.printf("%-5s %-20s %-12s %-22s %-24s %-12s %-10s\n",
+                                    "ID", "Title", "Release", "Genre", "Director", "Runtime", "Rating");
 
-                case 5:
-                    System.out.println("You are updating a movie rating");
-                    System.out.println("Enter the movie ID to update its rating: ");
-                    int movieUpdating = keyboard.nextInt();
-                    System.out.println("Enter the new rating for this movie: ");
-                    double newRating = keyboard.nextDouble();
-                    dao.updateRating(movieUpdating, newRating);
-                    System.out.println("Movie rating updated successfully");
+                            System.out.printf("%-5d %-20s %-12d %-22s %-24s %-12d %-10f\n",
+                                    foundMovie.getMovie_id(), foundMovie.getTitle(), foundMovie.getRelease_year(),
+                                    foundMovie.getGenre(), foundMovie.getDirector(),foundMovie.getRuntime_minutes(),foundMovie.getRating());
+                        }
+                        else
+                        {
+                            System.out.println("No Movie found with this ID!");
+                        }
 
-                    break;
+                        break;
 
-                case 6:
-                    System.out.println("Filtering Movies By Rating");
-                    System.out.println("Enter the minumum rating you want");
+                    case 4:
+                        System.out.println("Enter the movie ID to delete it: ");
+                        int movieIdToDelete = keyboard.nextInt();
+                        keyboard.nextLine();
+                        dao.deleteMovie(movieIdToDelete);
+                        System.out.println("Movie Deleted successfully");
 
-                    double minRating = keyboard.nextDouble();
-                    keyboard.nextLine();
+                        break;
 
-                    List<Movie> filteredMoviesbyRating = dao.filterMoviesByRating(minRating);
+                    case 5:
+                        System.out.println("You are updating a movie rating");
+                        System.out.println("Enter the movie ID to update its rating: ");
+                        int movieUpdating = keyboard.nextInt();
+                        System.out.println("Enter the new rating for this movie: ");
+                        double newRating = keyboard.nextDouble();
+                        dao.updateRating(movieUpdating, newRating);
+                        System.out.println("Movie rating updated successfully");
+
+                        break;
+
+                    case 6:
+                        System.out.println("Filtering Movies By Rating");
+                        System.out.println("Enter the minumum rating you want");
+
+                        double minRating = keyboard.nextDouble();
+                        keyboard.nextLine();
+
+                        List<Movie> filteredMoviesbyRating = dao.filterMoviesByRating(minRating);
 
                         if (!filteredMoviesbyRating.isEmpty()){
                             System.out.println("Filtered Movies");
@@ -116,54 +134,58 @@ public class MainApp {
                         else{
                             System.out.println("No Movies found above min rating");
                         }
+                        break;
 
-                    break;
+                    case 7:
+                        System.out.println("Which movies would you like to convert to Json format?");
+                        System.out.println("1.All Movies");
+                        System.out.println("2.By ID");
 
-                case 7:
-                    System.out.println("Which movies would you like to convert to Json format?");
-                    System.out.println("1.All Movies");
-                    System.out.println("2.By ID");
+                        String json;
+                        int op = keyboard.nextInt();
 
-                    String json;
-                    int op = keyboard.nextInt();
+                        if (op == 1) {
 
-                    if (op == 1) {
-
-                        List<Movie> allMovies = dao.getAllMovies();
-                        json = JsonConverter.moviesListToJson(allMovies);
-                        System.out.println("JSON Format: " + json);
-                    }
-
-                    else if (op == 2) {
-
-                        System.out.println("Please Enter the Movie ID");
-                        int movieIdJson = keyboard.nextInt();
-                        Movie foundMovieJson = dao.findMovieById(movieIdJson);
-
-                        if(foundMovieJson != null) {
-
-                            String movieJson = JsonConverter.gsonParser.toJson(foundMovieJson);
-
-                            json = JsonConverter.singleMovieToJson(movieJson);
-                            System.out.println("JSON representation of the found movie:");
-                            System.out.println(json);
+                            List<Movie> allMovies = dao.getAllMovies();
+                            json = JsonConverter.moviesListToJson(allMovies);
+                            System.out.println("JSON Format: " + json);
                         }
-                        else {
-                            System.out.println("No Movie found with this ID!");
+
+                        else if (op == 2) {
+
+                            System.out.println("Please Enter the Movie ID");
+                            int movieIdJson = keyboard.nextInt();
+                            Movie foundMovieJson = dao.findMovieById(movieIdJson);
+
+                            if(foundMovieJson != null) {
+
+                                String movieJson = JsonConverter.gsonParser.toJson(foundMovieJson);
+
+                                json = JsonConverter.singleMovieToJson(movieJson);
+                                System.out.println("JSON representation of the found movie:");
+                                System.out.println(json);
+                            }
+                            else {
+                                System.out.println("No Movie found with this ID!");
+                            }
                         }
-                    }
 
-                    break;
+                        break;
 
-                case 8:
-                    System.out.println("Exiting Code Now.");
+                    case 8:
 
-                    break;
-                default:
-                    System.out.println("Invalid Choice.");
-            }
-        } while (choice != 8); // Exits Loop
+
+                        break;
+                    case 9:
+                        System.out.println("Exiting Code Now.");
+                        break;
+                    default:
+                        System.out.println("Invalid Choice.");
+                }
+            } while (choice != 8); // Exits Loop
+        }
     }
+
 
     private static Movie insertNewMovie(Scanner keyboard) {
         Movie newMovie = new Movie();
